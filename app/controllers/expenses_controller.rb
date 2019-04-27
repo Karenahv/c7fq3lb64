8d9593082936087f9d75a33cceceea133f0162c5
karenahv <karenahv@gmail.com>
2019-04-27 15:13:30 -0500

    device

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
+    if user_signed_in?
      @expenses = Expense.order("date DESC")
+   else
+     ink_to "Sign up", new_user_registration_path
+   end
   end
 end
